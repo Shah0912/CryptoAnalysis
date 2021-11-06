b2a6e66d8e34a5efa20570ec860fa1cc6dade898
Shah0912 <adi.shah0912@gmail.com>
2021-11-06 23:06:14 +0530

    Merged frontend

---
+++ client-ui/src/pages/Tab2.tsx
@@ -0,0 +1,29 @@
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Frequency Analysis</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 2</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonItem>
+          <IonLabel position="stacked">Cipher Text</IonLabel>
+          <IonTextarea autoGrow={true}></IonTextarea>
+        </IonItem>
+        <IonButton>Analyze</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
